b1fc8ef525bc69d075b9918cdab00b6a2e6bb16e
klug2003 <muhammaddiyor.se@gmail.com>
2023-10-30 19:20:29 +0500

    increment and decrement

---
+++ my_first_increment.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+  int my_index = 0;
+
+  my_index++;
+  printf("%d\n", my_index);
+  my_index--;
+  my_index--;  
+  printf("%d\n", my_index);
+  my_index++;
+  my_index++;
+  my_index++;
+  
+  printf("%d\n", my_index);
+  return 0;
+}
